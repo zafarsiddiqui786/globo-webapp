1da9562345416ec11fd1dc9824f0852fe58bea79
Mohd Zafar Siddiqui <93350139+zafarsiddiqui786@users.noreply.github.com>
2023-09-03 19:04:43 +0530

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -120,7 +120,7 @@ resource "aws_lb_target_group" "main" {
   port        = 80
   target_type = "instance"
   protocol    = "HTTP"
-  vpc_id      = var.vpc_id
+  vpc_id      = data.tfe_outputs.networking.nonsensitive_values.public_subnets
 }
 
 resource "aws_alb_target_group_attachment" "main" {
