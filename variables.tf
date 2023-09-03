276b407bce3fff267dde2aa64745e2b435e7d6d5
Mohd Zafar Siddiqui <93350139+zafarsiddiqui786@users.noreply.github.com>
2023-09-03 18:59:03 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -37,14 +37,12 @@ variable "api_key" {
   description = "(Required) API key for web app to talk to SaaS platform."
 }
 
-variable "public_subnets" {
-  type        = list(string)
-  description = "(Required) List of subnet IDs for EC2 instance deployments."
-}
-
-variable "vpc_id" {
+variable "tfe_organization" {
   type        = string
-  description = "(Required) VPC ID of VPC for application deployment."
+  description = "(Required) Name of TFE Organization."
 }
 
-
+variable "tfe_workspace_name" {
+  type        = string
+  description = "(Required) API key for web app to talk to SaaS platform."
+}
